9426dd5317ef5e9cfe56092d9a5cdadb02e8011b
IshikawaTokyo <yyusb.mac@gmail.com>
2023-07-26 23:45:46 +0900

    done

---
+++ packages/frontend/.eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    tsconfigRootDir: __dirname,
+    project: ["tsconfig.eslint.json"],
+  },
+  plugins: ["@typescript-eslint", "react"],
+  rules: {},
+};
